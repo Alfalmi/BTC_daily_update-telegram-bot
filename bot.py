4376206acb4c805d504c39fa005c07bc9d371074
pinooxd <pinooxd@gmail.com>
2021-05-28 13:57:46 -0700

    btc_updates

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import schedule
 
 def bot_send_text(bot_message):
     
-    bot_token = '1700100216:AAESUHRnzKDF0iL_uHiMY2V33Ka88S-xCHU'
+    bot_token = ''
     bot_chatID = '1444121534'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
